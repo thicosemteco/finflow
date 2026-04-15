4d3251d7f264fc265468127a11b5657190bee284
Thiago <ns.thico@gmail.com>
2026-04-15 09:59:55 -0400

    Updates

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // FinFlow Service Worker
 // BUILD_TIMESTAMP is replaced at deploy time by deploy.sh
 // A new timestamp on every deploy forces cache invalidation and reload.
-const BUILD_TIMESTAMP = '20260415095045';
+const BUILD_TIMESTAMP = '20260415095952';
 const CACHE = 'finflow-' + BUILD_TIMESTAMP;
 
 const SHELL = [
